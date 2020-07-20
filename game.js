09554288f3369d80e1544766f9be4fc2c59a2da5
UVCoder1002 <uv6coder@gmail.com>
2020-07-20 22:37:58 +0530

    option selection and display editing

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -17,18 +17,26 @@ let questions = [
     answer: 1,
   },
   {
-    question: "Who invented computer",
+    question: "Who invented computer?",
     choice1: "Bjarne",
     choice2: "Linus Torwald",
     choice3: "Charles Babbage",
     choice4: "James Gosling",
     answer: 3,
   },
+  {
+    question: "Who invented C++ Language?",
+    choice1: "Bjarne",
+    choice2: "Linus Torwald",
+    choice3: "Charles Babbage",
+    choice4: "James Gosling",
+    answer: 1,
+  },
 ];
 console.log(questions);
 //CONSTANTS
 const CORRECT_BONUS = 4;
-const MAX_QUESTIONS = 2;
+const MAX_QUESTIONS = 3;
 
 startGame = () => {
   questionCounter = 0;
@@ -39,6 +47,9 @@ startGame = () => {
 };
 
 getNewQuestion = () => {
+  if (questionCounter >= MAX_QUESTIONS || availableQuestions.length == 0) {
+    return window.location.assign("/end.html");
+  }
   questionCounter++;
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
@@ -48,6 +59,27 @@ getNewQuestion = () => {
     const number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
   });
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
 };
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) {
+      return;
+    }
+    acceptingAnswers = false;
+    console.log(e);
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+    console.log(selectedAnswer, currentQuestion.answer);
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
+    selectedChoice.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.classList.remove(classToApply);
+      getNewQuestion();
+    }, 500);
+  });
+});
 startGame();
