0bf3171c40dbe5dddcb4a916ffc867e7546ddb4d
UVCoder1002 <uv6coder@gmail.com>
2020-07-26 00:13:45 +0530

    Highscorer list and page created

--- end.js
+++ end.js
@@ -2,6 +2,7 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreButton");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
+const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
 const finalScore = document.getElementById("finalScore");
 
 finalScore.innerText = mostRecentScore;
@@ -11,5 +12,15 @@ username.addEventListener("keyup", () => {
 });
 
 saveHighScore = (e) => {
-  console.log("button is clicked");
+  e.preventDefault();
+  // console.log("button is clicked");
+  const hScore = {
+    score: mostRecentScore,
+    username: username.value,
+  };
+  highScore.push(hScore);
+  highScore.sort((a, b) => b.score - a.score);
+  highScore.splice(5);
+  localStorage.setItem("highScore", JSON.stringify(highScore));
+  window.location = "index.html";
 };
